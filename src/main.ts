b0ad08e85fe97b43714927b08877f925cc60b5cc
Shaan Kumar <shaan123rock@gmail.com>
2023-01-20 12:13:44 +0530

    Github Authentication Project

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { env } from 'process';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  await app.listen(process.env.PORT || 5008);
+}
+bootstrap();
